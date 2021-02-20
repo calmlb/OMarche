e978fe7849902d789a1d97c159f436eb595fdb3f
Eric Mathias <emathias@gmail.com>
2021-02-19 22:56:42 -0500

    basic backend created - one test route for vendors

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/vendors', function(req,res) {
+    res.json({
+        vendors: []
+    })
+})
+
+module.exports = router;
