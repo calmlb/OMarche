6a51a477929e14fbeb5faef40a8a1092d14aeb79
Eric Mathias <emathias@gmail.com>
2021-02-25 10:37:32 -0500

    revert commit

--- controllers/store.js
+++ controllers/store.js
@@ -8,6 +8,7 @@ function CreateNewStore(req, res) {
 
     const store = new Store(req.body);
     store.save().then((store) => {
+        console.log(store);
         res.json(store);
     })
     .catch((err) => {
